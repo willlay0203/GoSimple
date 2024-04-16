c78233e6757751953f85248c9b0c560eeed76cea
William Lay <willlaydev@gmail.com>
2024-04-17 02:00:46 +1000

    Added routing http methods and added main file for testing

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module gokit
+module main
 
 go 1.22.0
--- gokit.go
+++ gokit.go
@@ -1,19 +1,39 @@
-package gokit
+package main
 
 import (
 	"net/http"
 )
 
-type Mux struct {
-	mux  *http.ServeMux
-	port string
+type Server struct {
+	Mux  *http.ServeMux
+	Port string
 }
 
-func Setup(port string) Mux {
-	m := Mux{
-		mux:  http.NewServeMux(),
-		port: port,
+func Setup(port string) Server {
+	m := Server{
+		Mux:  http.NewServeMux(),
+		Port: port,
 	}
 
 	return m
+}
+
+func (mux *Server) GET(p string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.Mux.HandleFunc("GET "+p, handler)
+}
+
+func (mux *Server) POST(p string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.Mux.HandleFunc("POST "+p, handler)
+}
+
+func (mux *Server) PUT(p string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.Mux.HandleFunc("PUT "+p, handler)
+}
+
+func (mux *Server) PATCH(p string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.Mux.HandleFunc("PATCH "+p, handler)
+}
+
+func (mux *Server) DELETE(p string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.Mux.HandleFunc("DELETE "+p, handler)
 }
